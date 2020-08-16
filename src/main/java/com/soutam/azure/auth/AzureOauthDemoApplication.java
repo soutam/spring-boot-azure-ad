87dff14d6a1182a5e6403111d63ba23cb1f9c355
soutam <soutampaul@gmail.com>
2020-08-16 19:27:10 +0530

    first commit

---
+++ src/main/java/com/soutam/azure/auth/AzureOauthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.soutam.azure.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureOauthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureOauthDemoApplication.class, args);
+	}
+
+}
